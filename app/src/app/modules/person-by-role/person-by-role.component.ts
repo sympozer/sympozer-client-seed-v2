82366e1fee406b1aec7bc0f0ed9b352858928013
Massi <m.bouneffa@upfit-services.com>
2017-03-24 15:51:03 +0100

    Style person by role

--- app/src/app/modules/person-by-role/person-by-role.component.ts
+++ app/src/app/modules/person-by-role/person-by-role.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Location }              from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Location}              from '@angular/common';
 import {LocalDAOService} from  '../../localdao.service';
 import {Encoder} from "../../lib/encoder";
+import {routerTransition} from '../../app.router.animation';
 
 @Component({
     selector: 'person-by-role',
     templateUrl: './person-by-role.component.html',
-    styleUrls: ['./person-by-role.component.css']
+    styleUrls: ['./person-by-role.component.css'],
+    animations: [routerTransition()],
+    host: {'[@routerTransition]': ''}
 })
 export class PersonByRoleComponent implements OnInit {
     persons;
+    personRole;
 
-    constructor(
-        private location: Location,
-        private route: ActivatedRoute,
-        private DaoService: LocalDAOService,  
-        private encoder: Encoder) {}
+    constructor(private location: Location,
+                private route: ActivatedRoute,
+                private DaoService: LocalDAOService,
+                private encoder: Encoder) {
+    }
 
     ngOnInit(): void {
         // propriété personLinkMapByRole
@@ -24,9 +28,11 @@ export class PersonByRoleComponent implements OnInit {
         this.route.params.forEach((params: Params) => {
             let id = params['id'];
             let name = params['ref'];
-            let query = { 'key' : this.encoder.decodeForURI(id) };
+            let query = {'key': this.encoder.decodeForURI(id)};
+            this.personRole = decodeURI(name);
+            console.log("Herrre", this.personRole);
             this.persons = this.DaoService.query("getPersonsByRole", query);
-            console.log("persons : " + JSON.stringify(this.persons)); 
+            console.log("persons : " + JSON.stringify(this.persons));
         });
     }
 }
