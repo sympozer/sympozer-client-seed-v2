ff48376599a4ffa4484c0bf07e9a1f3e8523f9d8
Massi <m.bouneffa@upfit-services.com>
2017-03-24 16:01:28 +0100

    Style categories for publication

--- app/src/app/modules/caterogies-for-publications/categories-for-publications.component.ts
+++ app/src/app/modules/caterogies-for-publications/categories-for-publications.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { LocalDAOService } from  '../../localdao.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {LocalDAOService} from  '../../localdao.service';
+import {routerTransition} from '../../app.router.animation';
+
 
 @Component({
     selector: 'categories-for-publications',
     templateUrl: './categories-for-publications.component.html',
-    styleUrls: ['./categories-for-publications.component.css']
+    styleUrls: ['./categories-for-publications.component.css'],
+    animations: [routerTransition()],
+    host: {'[@routerTransition]': ''}
 })
 export class CategoriesForPublicationsComponent implements OnInit {
     private categories = {};
-    constructor(
-        private DaoService : LocalDAOService) { }
+
+    constructor(private DaoService: LocalDAOService) {
+    }
 
     ngOnInit() {
         let allCat = this.DaoService.query("getAllCategoriesForPublications", null);
-        for(let i in allCat){
-            if(allCat[i] != null){
-                let query = { 'key' : i };
-                this.categories[i] = this.DaoService.query("getCategoryLink",query);
+        for (let i in allCat) {
+            if (allCat[i] != null) {
+                let query = {'key': i};
+                this.categories[i] = this.DaoService.query("getCategoryLink", query);
             }
         }
         console.log(this.categories);
