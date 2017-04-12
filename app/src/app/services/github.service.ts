7035a7a06db4d57877bd1d1ade7b59308f4405c5
fan_tad <fanoueltad@gmail.com>
2017-04-12 13:11:49 +0200

    Change token for github api

--- app/src/app/services/github.service.ts
+++ app/src/app/services/github.service.ts
@@ -2,14 +2,15 @@
  * Created by pierremarsot on 27/02/2017.
  */
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
+import {Http, Headers, RequestOptions} from "@angular/http";
+import * as FileSaver from 'file-saver';
 const github = require('octonode');
 
 @Injectable()
 export class GithubService {
-    private urlRepoGithub = 'octocat/Hello-World';
-    private lastHash = '762941318ee16e59dabbacb1b4049eec22f0d303';
-    private access_token = 'c9b1dbfd7d09c531cc60f14071ac520c24e9a2d3';
+    private urlRepoGithub = 'sympozer/sympozer-client-seed-v2';
+    private lastHash = 'dd11e471f9c2a278202bd4c7916643251b0d1329';
+    private access_token = '1dcc9dbbdf85a10cbcbe84c87abbb1f4255ab0b1';
     private clientGitHub = null;
     private repoGitHub = null;
 
@@ -128,6 +129,7 @@ export class GithubService {
 
                             //On récup le dernier commit (premier de la liste)
                             const lastCommit = response[0];
+                            console.log(response)
                             if (!lastCommit) {
                                 reject('Erreur lors de la récupération du dernier commit');
                                 return false;
@@ -135,7 +137,7 @@ export class GithubService {
 
                             //On regarde si on a la propriété "sha" dans l'objet du commit
                             if (!lastCommit.hasOwnProperty("sha")) {
-                                reject('Erreur lors de la récupération du sha du derneir commit');
+                                reject('Erreur lors de la récupération du sha du dernier commit');
                                 return false;
                             }
 
@@ -184,7 +186,7 @@ export class GithubService {
 
                             //Si le hash est égal au hash que l'on a en local, on stop car ça veut dire qu'on a pas eu d'update sur github
                             if (last_sha_commit === this.lastHash) {
-                                return reject();
+                                return reject("Sha commit is the same");
                             }
 
                             this.getRate()
@@ -222,7 +224,7 @@ export class GithubService {
         return new Promise((resolve, reject) => {
             try {
                 //On demande à github de faire une comparaison entre le commit que l'on a et la branche master
-                this.repoGitHub.compare(this.lastHash, 'master', (error, response) => {
+                this.repoGitHub.compare(this.lastHash, 'dev-front', (error, response) => {
                     if (error || !response) {
                         reject(error);
                         return false;
@@ -233,13 +235,16 @@ export class GithubService {
                         reject('Erreur lors de la récupération de l\'url de diff');
                         return false;
                     }
-
+                    console.log(response.diff_url)
                     //On télécharge le fichier de diff
+                    console.log(this.http)
+                    let headers = new Headers({ 'Access-Control-Allow-Origin': '*'});
+                    let options = new RequestOptions({ headers: headers });
                     this.http.get(response.diff_url)
                         .toPromise()
                         .then((response) => {
                             console.log('then diff url');
-                            console.log(response);
+                            console.log(response._body);
                             resolve(true);
                         })
                         .catch((error) => {
@@ -256,4 +261,53 @@ export class GithubService {
             }
         });
     };
+
+    parseDiffFileForEswc = (file) => {
+        return new Promise((resolve, reject) => {
+            console.log("in function")
+            var patt = new RegExp("^diff --git .*dataESWC.*d\n?")
+            var res = patt.test(file)
+            if(res){
+                let headers = new Headers({'Content-Type': 'application/json',});
+                let options = new RequestOptions({ headers: headers });
+                this.http.get("https://raw.githubusercontent.com/sympozer/sympozer-client-seed-v2/dev-front/app/src/app/conference_test.ttl")
+                    .toPromise()
+                    .then((response) => {
+                        console.log('raw');
+                        console.log(response);
+                        //this.extractContent(response)
+                        resolve(true);
+                    })
+                    .catch((error) => {
+                        console.log('err raw');
+                        console.log(error);
+                        reject('Erreur lors de la récupération du fichier de diff');
+                    });
+                //return true
+            }
+            console.log("returned false")
+            reject("returned false");
+        });
+    };
+
+    private extractContent(res) {
+        return new Promise((resolve, reject) => {
+            try{
+                /*let blob: Blob = res.blob();
+                 window['saveAs'](blob, 'test.txt');
+                 */
+                var blob = new Blob([res._body], { type: 'text/json' });
+                FileSaver.saveAs(blob, "test.json");
+                var url= window.URL.createObjectURL(blob);
+                window.open(url);
+                resolve("success creating blob")
+
+            }
+            catch(e){
+                reject(e)
+            }
+        });
+
+    };
+
 }
