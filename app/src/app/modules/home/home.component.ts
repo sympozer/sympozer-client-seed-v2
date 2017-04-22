8196e93eba8e3c73cf2c1b81f7dbea74bca17362
Massi <m.bouneffa@upfit-services.com>
2017-04-22 23:11:33 +0200

    Add twitter timeline

--- app/src/app/modules/home/home.component.ts
+++ app/src/app/modules/home/home.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import {routerTransition} from '../../app.router.animation';
 import {LocalDAOService} from '../../localdao.service';
 import {window} from "rxjs/operator/window";
+import {WindowReference} from '../../window-reference';
 
 @Component({
     selector: 'home',
@@ -17,7 +18,30 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-         if (document.getElementById("page-title-p"))
+        let win = WindowReference.get();
+
+        if (document.getElementById("page-title-p"))
             document.getElementById("page-title-p").innerHTML = "";
+
+        //
+        // let twttr = (function (d, s, id) {
+        //     var js, fjs = d.getElementsByTagName(s)[0],
+        //         t = twttr || {};
+        //     if (d.getElementById(id)) return t;
+        //     js = d.createElement(s);
+        //     js.id = id;
+        //     js.src = "https://platform.twitter.com/widgets.js";
+        //     fjs.parentNode.insertBefore(js, fjs);
+        //
+        //     t._e = [];
+        //     t.ready = function (f) {
+        //         t._e.push(f);
+        //     };
+        //
+        //     return t;
+        // }(document, "script", "twitter-wjs"));
+
+        win.twttr.widgets.load();
     }
+
 }
