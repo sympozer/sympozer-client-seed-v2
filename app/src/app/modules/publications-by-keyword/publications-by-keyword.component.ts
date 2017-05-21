bff640e996a0b9f101d86cdcd9f7c8fe878f4bff
pierremarsotlyon1 <pierre.marsot@etu.univ-lyon1.fr>
2017-05-22 00:54:03 +0200

    Rename html file

--- app/src/app/modules/publications-by-keyword/publications-by-keyword.component.ts
+++ app/src/app/modules/publications-by-keyword/publications-by-keyword.component.ts
@@ -1,23 +1,19 @@
-import {forEach} from "@angular/router/src/utils/collection";
 import {Component, OnInit} from "@angular/core";
-import {Conference} from "../../model/conference";
 import {Router, ActivatedRoute, Params} from "@angular/router";
-import {DataLoaderService} from "../../data-loader.service";
-import {DBLPDataLoaderService} from "../../dblpdata-loader.service";
 import {LocalDAOService} from "../../localdao.service";
 import {Encoder} from "../../lib/encoder";
 import {routerTransition} from '../../app.router.animation';
 
 @Component({
     selector: 'app-publication-by-keyword',
-    templateUrl: 'publications-by-keyword.html',
+    templateUrl: 'publications-by-keyword.component.html',
     styleUrls: ['publications-by-keyword.scss'],
     animations: [routerTransition()],
     host: {'[@routerTransition]': ''}
 })
 export class PublicationsByKeyword implements OnInit {
     title: string = "Publications by keyword";
-    public publications;
+    public publications = [];
 
     constructor(private router: Router, private route: ActivatedRoute,
                 private DaoService: LocalDAOService, private encoder: Encoder) {
@@ -48,7 +44,7 @@ export class PublicationsByKeyword implements OnInit {
                         if(id && label){
                             id = that.encoder.encode(id);
                             if(id){
-                                that.publications.push({
+                                that.publications = that.publications.concat({
                                     id: id,
                                     label: label,
                                 });
