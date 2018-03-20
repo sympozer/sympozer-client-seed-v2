7b0157672c7a2bf4fbe60f537036b248facae293
NGUON NICOLE p1408452 <nicole.nguon@etu.univ-lyon1.fr>
2018-03-20 15:49:57 +0100

    Sign up functional

--- app/src/app/modules/signup/signup.component.ts
+++ app/src/app/modules/signup/signup.component.ts
@@ -6,12 +6,14 @@ import {MdSnackBar} from "@angular/material";
 import {VoteService} from '../../services/vote.service'
 import {LocalDAOService} from "../../localdao.service";
 import {LocalStorageService} from 'ng2-webstorage';
+import {SignUpService} from './signup.service';
 
 
 @Component({
   selector: 'signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
+  providers: [SignUpService]
 })
 export class SignupComponent implements OnInit {
 
@@ -23,7 +25,8 @@ export class SignupComponent implements OnInit {
 	            public snackBar: MdSnackBar,
 	            private voteService: VoteService,
 	            private DaoService: LocalDAOService,
-	            private localStoragexx: LocalStorageService) { }
+				private localStoragexx: LocalStorageService,
+				private signUpService : SignUpService ) { }
 
 	ngOnInit() {
 		let user = this.localStoragexx.retrieve(this.key_localstorage_user)
@@ -37,10 +40,57 @@ export class SignupComponent implements OnInit {
   	/**
 	 * Invoke the signup external server service
 	 * @param email
+	 * @param firstname
+	 * @param lastname
 	 * @param password
 	 * @param confirm password
 	 */
-	signup(email, password, confirmPassword){
+	signup(email, firstname, lastname, password, confirmPassword){
+		
+		let result;
+		if(password == confirmPassword)
+		{
+			this.signUpService.register(email, firstname, lastname, password).subscribe(
+
+				response => {
+					result = response;
+				},
+
+				err => { 
+					console.log(err);
+				},
+
+				() => { 
+					this.snackBar.open("Please check your email to validate your account.", "", {
+						duration: 7000,
+					});
+					this.snackBar.open("The account creation request has been accepted by our server.", "", {
+						duration: 4000,
+					});
+					let urlHost = window.location.protocol+'//'+window.location.host + window.location.pathname
+            		window.location.replace(urlHost+'#/login');
+				}
+
+
+
+			)
+		}
+		else if (password != confirmPassword)
+		{
+			this.snackBar.open("Your password must be the same. ", "", {
+				duration: 4000
+			});
+		}
+		else
+		{
+			this.snackBar.open("Your password must be longer than 8 characters. ", "", {
+				duration: 4000
+			});
+		}
+		
+
+
+		/*
 		const that = this
 	    this.apiExternalServer.signup(email, password, confirmPassword)
 	        .then(() => {
@@ -61,7 +111,8 @@ export class SignupComponent implements OnInit {
 	            this.snackBar.open(err, "", {
 	                duration: 2000,
 	            });
-	        });
+			});
+			*/
 	}
 
 }
