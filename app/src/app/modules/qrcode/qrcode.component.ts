348e6c78a6fd73744407013a44c6e559643ff9ba
Nil <nilsahin58@gmail.com>
2018-04-09 15:36:42 +0200

    update qrcode url

--- app/src/app/modules/qrcode/qrcode.component.ts
+++ app/src/app/modules/qrcode/qrcode.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {LocalStorageService} from 'ng2-webstorage';
 
 @Component({
     selector: 'app-qrcode',
     templateUrl: 'qrcode.component.html'
 })
 export class QrcodeComponent {
-    elementType : 'url' | 'canvas' | 'img' = 'url';
-    value : string = 'Techiediaries';
+    elementType: 'url' | 'canvas' | 'img' = 'url';
+    private key_localstorage_user = "user_external_ressource_sympozer";
+    private id_localstorage_user = "id_external_ressource_sympozer";
+    qrValue;
+
+
+    constructor(private localStoragexx: LocalStorageService) {
+    }
+
+    ngOnInit() {
+        let user = this.localStoragexx.retrieve(this.key_localstorage_user);
+        let id = this.localStoragexx.retrieve(this.id_localstorage_user);
+        console.log(user.firstname);
+        this.qrValue = 'https://sympozer.liris.cnrs.fr/www2018/#/person/' + user.firstname + '/' + id;
+    }
+
+    //value: string = 'https://sympozer.liris.cnrs.fr/www2018/#/person/' + this.user.firstname + '/' + this.key_localstorage_user;
+
+    //value: string = 'https://sympozer.liris.cnrs.fr/www2018/#/'
 }
