42612a71952033c7f8727265c5d2725accfcd257
Nguyen Van Thanh Tu <nguyenvanthanhtu@gmail.com>
2018-04-19 14:50:52 +0200

    Starting new stuff with participant

---
+++ app/src/app/modules/participant/participant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'participant',
+  templateUrl: './participant.component.html',
+  styleUrls: ['./participant.component.scss']
+})
+export class ParticipantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
