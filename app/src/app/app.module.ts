55526941d32c1dfaf34baa1496cc0ecd47192938
LAO Jérémy <jeremy.lao@etu.univ-lyon1.fr>
2018-04-20 11:18:15 +0200

    removed social login file

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,7 +6,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {routing} from './app.routing';
-import {MaterialModule, MdGridListModule, MdDialogModule} from '@angular/material';
+import {MaterialModule, MdGridListModule} from '@angular/material';
 import 'hammerjs';
 import {NgModule} from '@angular/core';
 import {HomeComponent} from './modules/home/home.component';
@@ -66,7 +66,6 @@ import {UserProfileComponent} from './modules/user-profile/user-profile.componen
 import {ForgotPasswordComponent} from './modules/forgotPassword/forgotPassword.component';
 import {ActivationMailComponent} from './modules/activationMail/activationMail.component';
 import {ChangePasswordComponent} from './modules/changePassword/changePassword.component';
-import {SocialloginComponent} from './modules/sociallogin/sociallogin.component';
 
 const routes: Routes = [];
 
@@ -116,7 +115,6 @@ const routes: Routes = [];
         ForgotPasswordComponent,
         ActivationMailComponent,
         ChangePasswordComponent,
-        SocialloginComponent
     ],
     imports: [
         BrowserModule,
@@ -128,7 +126,6 @@ const routes: Routes = [];
         RouterModule.forRoot(routes, {useHash: true}),
         MaterialModule,
         MdGridListModule,
-        MdDialogModule,
         ShareButtonsModule.forRoot(),
         Ng2Webstorage,
         Angulartics2Module.forRoot([Angulartics2Piwik]),
@@ -149,7 +146,6 @@ const routes: Routes = [];
         TimeManager
     ],
     bootstrap: [AppComponent],
-    entryComponents:[SocialloginComponent]
 })
 export class AppModule {
 }
