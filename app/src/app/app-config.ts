0acc4864bd04a55e5d18d2a03017a353386d5af8
Pierre-Antoine Champin <pchampin@liris.cnrs.fr>
2018-04-26 07:34:16 +0200

    vote server

--- app/src/app/app-config.ts
+++ app/src/app/app-config.ts
@@ -88,7 +88,7 @@ export let Config = {
         url: 'https://sympozer.com/external'
     },
     vote :{
-        url: 'http://localhost:3000',
+        url: 'https://sympozer.com/vote',
         tracks: ['https://w3id.org/scholarlydata/track/www-demo-2018', 'https://w3id.org/scholarlydata/track/www-posters-2018']
     }
 };
