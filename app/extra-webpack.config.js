1d7c5f056951fbe1399cbd866c9ff2b6c1fda3f3
Nour MEDJEDEL <nour-el-houda.medjedel@etu.univ-lyon1.fr>
2019-03-26 11:44:02 +0100

    premiere version

---
+++ app/extra-webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  node: {
+    crypto: true,
+    http: true,
+    https: true,
+    os: true,
+    vm: true,
+    stream: true, 
+    fs: 'empty', 
+    net: 'empty',
+    tls: 'empty', 
+    child_process: 'empty'
+  }
+}
